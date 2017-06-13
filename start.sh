ab12f0a66e28f71f62daa4e22d2d557df1b1ccc0
aakos23 <akos.asztalos@gmail.com>
2017-06-13 10:48:11 +0200

    fixed file exists error

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+file="/var/run/dbus/pid"
+
+if [ -f $file ]; then rm $file; fi
 mkdir -p /var/run/dbus
 dbus-daemon --system && avahi-daemon -D && shairport-sync -m avahi -a "$AIRPLAY_NAME" "$@"
